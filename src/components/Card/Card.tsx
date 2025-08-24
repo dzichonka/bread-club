34eb2802c2b44a9fa6143292dcc687094be627c4
Anna Vasilevich <dzichonka@gmail.com>
2025-08-24 22:53:23 +0200

    feat: add validation for Uncontrolled Form

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,11 +6,7 @@ const Card = (data: IFormData): React.JSX.Element => {
   return (
     <div className="relative flex flex-row gap-3 justify-between bg-gray-950/50 p-4 rounded">
       <h2 className="flex flex-col justify-center items-center text-lg font-bold">
-        <img
-          src={picture || '/images/bread.png'}
-          alt={name}
-          className="w-16 h-16"
-        />
+        <img src={picture || '/images/bread.png'} alt={name} className="w-16" />
         {name}
       </h2>
       <h3 className="flex flex-col justify-center items-center">
--- src/components/UncontrolledForm/UncontrolledForm.tsx
+++ src/components/UncontrolledForm/UncontrolledForm.tsx
@@ -1,8 +1,8 @@
-//import { schema, type FormSchema } from '@/validation/formSchema';
+import { schema } from '@/validation/formSchema';
 import { useCountryStore } from '@/store/useCountryStore';
 import { useFormStore } from '@/store/useFormStore';
-import type { Gender } from '@/types/types';
-import { useRef, type FormEvent } from 'react';
+import type { Gender, IFormData } from '@/types/types';
+import { useRef, useState, type FormEvent } from 'react';
 
 type UncontrolledFormProps = {
   onClose: () => void;
@@ -12,6 +12,7 @@ const UncontrolledForm = ({ onClose }: UncontrolledFormProps) => {
   const { addUser } = useFormStore();
   const { countries } = useCountryStore();
   const formRef = useRef<HTMLFormElement>(null);
+  const [errors, setErrors] = useState<Record<string, string>>({});
 
   const fileToBase64 = (file: File): Promise<string> => {
     return new Promise((resolve, reject) => {
@@ -24,19 +25,18 @@ const UncontrolledForm = ({ onClose }: UncontrolledFormProps) => {
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const form = e.target as HTMLFormElement;
+    const form = e.currentTarget;
 
     const fileInput = form.elements.namedItem('picture') as HTMLInputElement;
     const file = fileInput.files?.[0];
-
     let pictureBase64 = '';
     if (file) {
       pictureBase64 = await fileToBase64(file);
     }
 
-    addUser({
+    const formData = {
       name: (form.elements.namedItem('name') as HTMLInputElement).value,
-      age: Number((form.elements.namedItem('age') as HTMLInputElement).value),
+      age: (form.elements.namedItem('age') as HTMLInputElement).value,
       email: (form.elements.namedItem('email') as HTMLInputElement).value,
       password: (form.elements.namedItem('password') as HTMLInputElement).value,
       confirmPassword: (
@@ -44,12 +44,34 @@ const UncontrolledForm = ({ onClose }: UncontrolledFormProps) => {
       ).value,
       gender: (form.elements.namedItem('gender') as HTMLInputElement)
         .value as Gender,
-      picture: pictureBase64,
+      picture: fileInput.files,
       acceptTnC: (form.elements.namedItem('acceptTnC') as HTMLInputElement)
         .checked,
       country: (form.elements.namedItem('country') as HTMLInputElement).value,
-    });
+    };
 
+    const result = schema.safeParse(formData);
+
+    if (!result.success) {
+      const formErrors: Record<string, string> = {};
+      result.error.issues.forEach((err) => {
+        if (err.path[0]) {
+          formErrors[err.path[0] as string] = err.message;
+        }
+      });
+      setErrors(formErrors);
+      return;
+    }
+
+    const data: IFormData = {
+      ...formData,
+      age: Number(formData.age),
+      gender: formData.gender as Gender,
+      picture: pictureBase64,
+    };
+
+    addUser(data);
+    setErrors({});
     form.reset();
     setTimeout(onClose, 500);
   };
@@ -66,32 +88,38 @@ const UncontrolledForm = ({ onClose }: UncontrolledFormProps) => {
 
       <div className="flex flex-col">
         <label htmlFor="name">Name</label>
-        <input className="btn pt-2" type="text" id="name" name="name" />
-        {/* {errors.name && (
+        <input
+          className="btn pt-2"
+          type="text"
+          id="name"
+          name="name"
+          required
+        />
+        {errors.name && (
           <span className="text-xs text-amber-100 h-0 text-end">
-            {errors.name.message}
+            {errors.name}
           </span>
-        )} */}
+        )}
       </div>
 
       <div className="flex flex-col">
         <label htmlFor="age">Age</label>
-        <input className="btn pt-2" type="text" id="age" name="age" />
-        {/* {errors.age && (
+        <input className="btn pt-2" type="text" id="age" name="age" required />
+        {errors.age && (
           <span className="text-xs text-amber-100 h-0 text-end">
-            {errors.age.message}
+            {errors.age}
           </span>
-        )} */}
+        )}
       </div>
 
       <div className="flex flex-col">
         <label htmlFor="email">Email</label>
-        <input className="btn pt-2" type="email" id="email" />
-        {/* {errors.email && (
+        <input className="btn pt-2" type="email" id="email" required />
+        {errors.email && (
           <span className="text-xs text-amber-100 h-0 text-end">
-            {errors.email.message}
+            {errors.email}
           </span>
-        )} */}
+        )}
       </div>
 
       <div className="flex flex-col">
@@ -101,12 +129,13 @@ const UncontrolledForm = ({ onClose }: UncontrolledFormProps) => {
           type="password"
           id="password"
           name="password"
+          required
         />
-        {/* {errors.password && (
+        {errors.password && (
           <span className="text-xs text-amber-100 h-0 text-end">
-            {errors.password.message}
+            {errors.password}
           </span>
-        )} */}
+        )}
       </div>
 
       <div className="flex flex-col">
@@ -116,26 +145,27 @@ const UncontrolledForm = ({ onClose }: UncontrolledFormProps) => {
           type="password"
           id="confirmPassword"
           name="confirmPassword"
+          required
         />
-        {/* {errors.confirmPassword && (
+        {errors.confirmPassword && (
           <span className="text-xs text-amber-100 h-0 text-end">
-            {errors.confirmPassword.message}
+            {errors.confirmPassword}
           </span>
-        )} */}
+        )}
       </div>
 
       <div className="flex flex-col">
         <label htmlFor="gender">Gender</label>
-        <select className="btn" id="gender" name="gender">
+        <select className="btn" id="gender" name="gender" required>
           <option value="male">male</option>
           <option value="female">female</option>
           <option value="other">other</option>
         </select>
-        {/* {errors.gender && (
+        {errors.gender && (
           <span className="text-xs text-amber-100 h-0 text-end">
-            {errors.gender.message}
+            {errors.gender}
           </span>
-        )} */}
+        )}
       </div>
 
       <div className="flex flex-col">
@@ -146,17 +176,18 @@ const UncontrolledForm = ({ onClose }: UncontrolledFormProps) => {
           accept="image/png, image/jpeg"
           id="picture"
           name="picture"
+          required
         />
-        {/* {errors.picture && (
+        {errors.picture && (
           <span className="text-xs text-amber-100 h-0 text-end">
-            {errors.picture.message}
+            {errors.picture}
           </span>
-        )} */}
+        )}
       </div>
 
       <div className="flex flex-col">
         <label htmlFor="country">Country</label>
-        <select id="country" className="btn" name="country">
+        <select id="country" className="btn" name="country" required>
           <option value="">-- Select a country --</option>
           {countries.map((c) => (
             <option key={c} value={c}>
@@ -164,11 +195,11 @@ const UncontrolledForm = ({ onClose }: UncontrolledFormProps) => {
             </option>
           ))}
         </select>
-        {/* {errors.country && (
+        {errors.country && (
           <span className="text-xs text-amber-100 h-0 text-end">
-            {errors.country.message}
+            {errors.country}
           </span>
-        )} */}
+        )}
       </div>
 
       <div className="flex flex-col">
@@ -181,16 +212,17 @@ const UncontrolledForm = ({ onClose }: UncontrolledFormProps) => {
             type="checkbox"
             className="peer hidden"
             name="acceptTnC"
+            required
           />
           <div className="w-5 h-5 bg-black border-2 border-amber-300 rounded-full flex items-center justify-center peer-checked:bg-yellow-400"></div>
           Accept Terms and Conditions
         </label>
-        {/*
+
         {errors.acceptTnC && (
           <span className="text-xs text-amber-100 h-0 text-end">
             You must accept T&C
           </span>
-        )} */}
+        )}
       </div>
 
       <button type="submit" className="btn">
